c6da868494615cfa22731f409ee0e36bf20e15b9
JustSunr1se <stas.schegolkov@gmail.com>
2021-05-24 09:38:55 +0300

    created new components

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://i.pinimg.com/originals/31/87/3d/31873dc18a8340047aa0681393e9103b.png" alt="Logo"/>
+        </header>
+    )
+}
+export default Header;
